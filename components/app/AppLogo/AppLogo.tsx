b28fe4e5d7eb56759297fe304b542b612de2bd24
CaiYiLiang <cherryliang0112@gmail.com>
2022-10-03 11:22:12 +1300

    fix: caseSensitive issue

--- components/app/AppLogo/AppLogo.tsx
+++
@@ -1,8 +0,0 @@
-// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import * as React from 'react';
-
-export const Logo: React.FC = () => {
-  return <div>Logo</div>;
-};
