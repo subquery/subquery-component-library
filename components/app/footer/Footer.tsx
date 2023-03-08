4e41a8a1cc51b7d04d39d0cfe149f5b7ccf43913
CaiYiLiang <cherryliang0112@gmail.com>
2023-03-08 16:10:54 +1300

    feat: imporve props for Footer

--- components/app/footer/Footer.tsx
+++ components/app/footer/Footer.tsx
@@ -94,7 +94,11 @@ const Simple: React.FC = () => {
   );
 };
 
+<<<<<<< HEAD
 export interface IFooter {
+=======
+interface IFooter {
+>>>>>>> d9e1923 (feat: imporve props for Footer)
   simple?: boolean;
 }
 
