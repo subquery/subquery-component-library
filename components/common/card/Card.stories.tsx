54c2fe64c1ae407189f0d110c066d0660e8c6b62
CaiYiLiang <cherryliang0112@gmail.com>
2023-03-31 15:36:03 +1300

    feat:rm comment code

--- components/common/card/Card.stories.tsx
+++ components/common/card/Card.stories.tsx
@@ -3,13 +3,11 @@
 
 import { Card } from './Card';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-// import { InfoCircleOutlined } from '@ant-design/icons';
 import cardIcon from '../../../assets/Card-logo.svg';
 
 const description = '0 SQT';
 const title = 'You are DELEGATING';
 const titleTooltip = 'The amount of kSQT that you are delegating to Indexers in the SubQuery Network to earn rewards';
-// const titleTooltipIcon = <InfoCircleOutlined />;
 const icon = cardIcon;
 const dropdown = {
   items: [
